8d236c2f188b311aba307f692120a315ca438400
Nguyen Truong <truongvinhnhunguyen@gmail.com>
2017-03-30 22:17:13 +0700

    adding Switch

--- app/src/main/java/com/aiohtea/aiohteacollection/MainActivity.java
+++ app/src/main/java/com/aiohtea/aiohteacollection/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aiohtea.aiohteacollection;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,15 +45,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         listView.setOnItemClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Class to listen Floating Action Button
+        class Xxx implements View.OnClickListener{
+            MainActivity m_t;
+
+            Xxx(MainActivity t){ m_t = t;}
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+              Intent intent = new Intent(m_t, SwitchAdding.class);
+              startActivity(intent);
             }
-        });
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new Xxx(this));
     }
 
     @Override
---
+++ app/src/main/java/com/aiohtea/aiohteacollection/SwitchAdding.java
@@ -0,0 +1,13 @@
+package com.aiohtea.aiohteacollection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwitchAdding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.switch_adding);
+    }
+}
