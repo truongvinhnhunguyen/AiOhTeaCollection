18bf806a6555480abebbfc7b5a91c03c3133bb73
Nguyen Truong <truongvinhnhunguyen@gmail.com>
2017-03-30 16:25:20 +0700

    have sample of picture list

--- app/src/main/java/com/aiohtea/aiohteacollection/MainActivity.java
+++ app/src/main/java/com/aiohtea/aiohteacollection/MainActivity.java
@@ -1,24 +1,51 @@
 package com.aiohtea.aiohteacollection;
 
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    List<DeviceListItem> m_swList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
+        m_swList = new ArrayList<DeviceListItem>();
+
+        for(int i = 0; i<20; i++){
+            m_swList.add(new SwitchListItem("Switch" + i, "Description of: "+i, i%2));
+        }
+
+        ListView listView = (ListView)findViewById(R.id.device_list);
+
+        listView.setAdapter(new DeviceListViewAdapter(this, R.layout.device_list_item, m_swList));
+
+        listView.setOnItemClickListener(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Item " + (position + 1) + ": " + m_swList.get(position),
+                Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
     }
 
     @Override
